89b22e7cdd839d91d119b5c9980b0b2d36724f28
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2024-09-14 12:56:10 +0700

    fix: fix client render bug

--- src/components/Buttons/UpdateStatusButtons.tsx
+++ src/components/Buttons/UpdateStatusButtons.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import { updateRegistrationStatus } from "@/utils/action";
-import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function UpdateStatusButtons({ id }: { id: string }) {
   const router = useRouter();
   const handleClick = async (status: string) => {
-    const session = await getServerSession();
-    if (!session) {
-      return null;
-    }
     const updatedRegistration = await updateRegistrationStatus(id, status);
     if (updatedRegistration) {
       alert(`User is ${status}ed`);
