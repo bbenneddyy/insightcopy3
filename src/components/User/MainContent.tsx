ada20cf0b367d044da13cc971d446a78e301d43f
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-11-01 19:51:31 +0700

    feat: add zoom meeting ID and password

--- src/components/User/MainContent.tsx
+++ src/components/User/MainContent.tsx
@@ -29,15 +29,20 @@ export default function MainContent({ site }: IMainContent) {
         <div className="w-1/2 m-4 p-6 bg-white rounded-lg shadow-lg border border-gray-200">
           <div className="text-center mb-4">
             {site === "online" &&
-              <h1 className="text-lg font-semibold text-blue-500">
-                <a href="https://chula.zoom.us/j/98602876220?pwd=H2XMmg1qnT2x00UayarfKkUdz1mpqT.1"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  กดเพื่อเข้าร่วมงานเสวนา
-                  <LinkIcon className="h-5 w-5 inline" />
-                </a>
-              </h1>}
+              <>
+                <h1 className="text-lg font-semibold text-blue-500">
+                  <a href="https://chula.zoom.us/j/98602876220?pwd=H2XMmg1qnT2x00UayarfKkUdz1mpqT.1"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    กดเพื่อเข้าร่วมงานเสวนา
+                    <LinkIcon className="h-5 w-5 inline" />
+                  </a>
+                </h1>
+                <p>Meeting ID: 986 0287 6220</p>
+                <p>Password: 626147</p>
+              </>
+            }
             <h3 className="text-gray-500">
               วีดีโอย้อนหลังและเกียรติบัตรการเข้าร่วม
               จะสามารถใช้ได้หลังสิ้นสุดงานเสวนา
