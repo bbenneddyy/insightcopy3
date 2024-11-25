09e4dc04151467b6ada62334348ef64dc85a1021
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-11-25 16:20:00 +0700

    feat: change ... to spinner (#19)

--- src/components/Certificate/DownloadCertificate.tsx
+++ src/components/Certificate/DownloadCertificate.tsx
@@ -36,7 +36,7 @@ export default function DownloadCertificate({
       const thaiFontBytes = await fetch("/font/NotoSansThai-Regular.ttf").then(
         (res) => res.arrayBuffer()
       );
-      const thaiFont = await pdfDoc.embedFont(thaiFontBytes, {subset: true});
+      const thaiFont = await pdfDoc.embedFont(thaiFontBytes, { subset: true });
 
       // Get the first page of the PDF template
       const [templatePage] = pdfDoc.getPages();
@@ -88,7 +88,15 @@ export default function DownloadCertificate({
             className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 w-1/2"
             onClick={handleDownloadCertificate}
           >
-            {isDownloading ? "..." : "ดาวน์โหลดเกียรติบัตร"}
+            {isDownloading ?
+            <span className="flex justify-center">
+              <svg className="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+              </span>
+              :
+              "ดาวน์โหลดเกียรติบัตร"}
           </button>
         </div>
       )}
